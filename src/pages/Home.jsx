c39cecf2df7688c76c35b7294c4aac9c97c86e39
Rafi Kadir <rafikadir>
2023-06-01 22:18:56 +0600

    section title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar/Navbar";
 import Promo from "../components/PromoBar/Promo";
+import SectionTitle from "../components/SectionTitle/SectionTitle";
 import Sliders from "../components/Sliders/Sliders";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
             <Promo/>
             <Navbar/>
             <Sliders/>
+            <SectionTitle title="Featured Products"/>
         </>
     );
 };
