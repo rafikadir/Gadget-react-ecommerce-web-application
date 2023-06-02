95502fcc5575c06fd207f3d22df45db7d0b5d9ec
rafikadir <rafikadir2000@gmail.com>
2023-06-02 11:33:42 +0600

    add product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,13 @@
 import './Product.scss';
+import pd from '../../assets/img/products/1.png'
 
 const Product = () => {
     return (
-        <div>
-            test
+        <div className="product-box">
+            <div className="product-img">
+                <img src={pd} alt="" />
+            </div>
+            <div></div>
         </div>
     );
 };
