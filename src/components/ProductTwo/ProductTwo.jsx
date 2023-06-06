f82d7592110dd57dd12778ebd26a3a63cd402659
rafikadir <rafikadir2000@gmail.com>
2023-06-07 02:04:09 +0600

    new arrival

--- src/components/ProductTwo/ProductTwo.jsx
+++ src/components/ProductTwo/ProductTwo.jsx
@@ -4,11 +4,14 @@ import { Link } from 'react-router-dom';
 import { AiOutlineShoppingCart, AiOutlineHeart } from "react-icons/ai";
 
 
-const ProductTwo = () => {
+const ProductTwo = (product) => {
+
+    const {title, img, price, prevPrice} = product.product;
+
     return (
         <div className="product-two-box">
             <div className="product-img">
-                <img src={pd} alt="" />
+                <img src={img} alt="" />
 
                 <div className="product-extra">
                     <button className="product-cart">
@@ -20,8 +23,8 @@ const ProductTwo = () => {
                 </div>
             </div>
             <div className="product-text">
-                <h3><Link className='product-title' to="/">Cantilever chair</Link></h3>
-                <span className='product-price'>$26 <del>$68</del></span>
+                <h3><Link className='product-title' to="/">{title}</Link></h3>
+                <span className='product-price'>${price} <del>${prevPrice}</del></span>
             </div>
         </div>
     );
