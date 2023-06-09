b229cfab8ab7ce15c09dd0748c6e08fd07468222
rafikadir <rafikadir2000@gmail.com>
2023-06-09 22:42:31 +0600

    footer structure

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -6,23 +6,61 @@ import { AiFillFacebook, AiFillInstagram, AiFillTwitterSquare } from "react-icon
 
 const Footer = () => {
     return (
-        <footer className="footer">
+        <footer>
             <div className="container">
                 <div className="row">
-                    <div className="compnay-info">
-                        <img src={logo} alt="logo" />
+                    <div className="col-lg-3">
+                        <div className="compnay-info">
+                            <img src={logo} alt="logo" />
 
-                        <ul>
-                            <li>17 Princess Road, London</li>
-                            <li>furnish@gmail.com</li>
-                            <li>+0 998 5585 </li>
-                        </ul>
+                            <ul>
+                                <li>17 Princess Road, London</li>
+                                <li>furnish@gmail.com</li>
+                                <li>+0 998 5585 </li>
+                            </ul>
 
-                        <ul>
-                            <li><Link to="/"><AiFillFacebook/></Link></li>
-                            <li><Link to="/"><AiFillInstagram/></Link></li>
-                            <li><Link to="/"><AiFillTwitterSquare/></Link></li>
-                        </ul>
+                            <div>
+                                <Link to="/"><AiFillFacebook/></Link>
+                                <Link to="/"><AiFillInstagram/></Link>
+                                <Link to="/"><AiFillTwitterSquare/></Link>
+                            </div> 
+                        </div>
+                    </div>
+
+                    <div className="col-lg-3">
+                        <div className="footer-links">
+                            <h3>Catagories</h3>
+                            <ul>
+                                <li><Link to="/">Chair</Link></li>
+                                <li><Link to="/">Sofa</Link></li>
+                                <li><Link to="/">Table</Link></li>
+                                <li><Link to="/">Book Shelf</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-3">
+                        <div className="footer-links">
+                            <h3>Customer Care</h3>
+                            <ul>
+                                <li><Link to="/">My Account</Link></li>
+                                <li><Link to="/">Returns</Link></li>
+                                <li><Link to="/">Orders History</Link></li>
+                                <li><Link to="/">Order Tracking</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-3">
+                        <div className="footer-links">
+                            <h3>Company</h3>
+                            <ul>
+                                <li><Link to="/">About</Link></li>
+                                <li><Link to="/">Shop</Link></li>
+                                <li><Link to="/">Blog</Link></li>
+                                <li><Link to="/">Contact</Link></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
