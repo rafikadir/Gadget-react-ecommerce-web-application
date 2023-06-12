f0f77c5b6671cfad44fa05f99d5728feeeb55e80
rafikadir <rafikadir2000@gmail.com>
2023-06-12 23:58:07 +0600

    slider image

--- src/components/Sliders/Sliders.jsx
+++ src/components/Sliders/Sliders.jsx
@@ -2,8 +2,8 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import "./Sliders.scss";
-import img from '../../assets/img/slider/2.png';
 import ThemeBtn from "../ThemeButton/ThemeBtn";
+import slidersData from '../../data/slider.json';
 
 const Sliders = () => {
 
@@ -20,65 +20,28 @@ const Sliders = () => {
         <section className="slider-wrapper">
             <div className="container-fluid p-0">
                 <Slider {...settings}>
-                    <div className="single-slider">
-                        <div className="container">
-                            <div className="row align-items-center">
-                                <div className="col-lg-6">
-                                    <div className="slider-text">
-                                        <span>Best Furniture For Your Castle....</span>
-                                        <h2>New Furniture Collection Trends in 2020</h2>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing
-                                        in phasellus non in justo.</p>
-                                        
-                                        <ThemeBtn name="Shop Now" link="/"/>
+                    {
+                       slidersData.map((item, index)=>
+                        <div className="single-slider" key={index}>
+                            <div className="container">
+                                <div className="row align-items-center">
+                                    <div className="col-lg-6">
+                                        <div className="slider-text">
+                                            <span>{item.subtitle}</span>
+                                            <h2>{item.title}</h2>
+                                            <p>{item.description}</p>
+                                            
+                                            <ThemeBtn name="Shop Now" link={item.link}/>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="col-lg-6">
-                                    <img className="slider-img" src={img} alt="chair"/>
+                                    <div className="col-lg-6">
+                                        <img className="slider-img" src={item.image} alt="product"/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-
-                    <div className="single-slider">
-                        <div className="container">
-                            <div className="row align-items-center">
-                                <div className="col-lg-6">
-                                    <div className="slider-text">
-                                        <span>Best Furniture For Your Castle....</span>
-                                        <h2>New Furniture Collection Trends in 2020</h2>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing
-                                        in phasellus non in justo.</p>
-                                        
-                                        <ThemeBtn name="Shop Now" link="/"/>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6">
-                                    <img className="slider-img" src={img} alt="chair"/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="single-slider">
-                        <div className="container">
-                            <div className="row align-items-center">
-                                <div className="col-lg-6">
-                                    <div className="slider-text">
-                                        <span>Best Furniture For Your Castle....</span>
-                                        <h2>New Furniture Collection Trends in 2020</h2>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing
-                                        in phasellus non in justo.</p>
-                                        
-                                        <ThemeBtn name="Shop Now" link="/"/>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6">
-                                    <img className="slider-img" src={img} alt="chair"/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                        ) 
+                    }
                 </Slider>
             </div>
         </section>
