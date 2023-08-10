acc74c2af74aafcac068442394a3e5581fe3a96e
rafikadir <rafikadir2000@gmail.com>
2023-08-11 01:52:34 +0600

    firebase add

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -2,28 +2,37 @@ import "./signin.scss";
 import {createUserWithEmailAndPassword} from "firebase/auth";
 import {auth} from '../../auth/firebase';
 import {useState} from "react";
+import {FcGoogle} from "react-icons/fc";
 
-
-const signin = () => {
-    const [isLoggedIn, SetIsLoggedIn] = useState(false);
+const Signin = () => {
+    const [isSignedIn, SetIsSignedIn] = useState(false);
     const [user, setUser] = useState({
         name: "",
         email: "",
-        password: "" 
+        password: "",
+        passwordCheck: ""
     });
 
     const handleSubmit = (e) => {
-        if(user.email && user.password) {
-            createUserWithEmailAndPassword(auth, user.email, user.password)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                console.log(user)
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorCode,errorMessage);
-            });
+        
+        if(user.password === user.passwordCheck){
+            console.log("Match");
+        }
+        else {
+            console.log("Not Matched");
+        }
+
+        if(user.email && user.password){
+            // createUserWithEmailAndPassword(auth, user.email, user.password)
+            // .then((userCredential) => {
+            //     const user = userCredential.user;
+            //     console.log(user);
+            // })
+            // .catch((error) => {
+            //     const errorCode = error.code;
+            //     const errorMessage = error.message;
+            //     console.log(errorCode,errorMessage);
+            // });
         }
         e.preventDefault();
     }
@@ -34,24 +43,59 @@ const signin = () => {
         setUser(newUser);
     }
 
+    const toggleSignin = () => {
+        SetIsSignedIn(!isSignedIn);
+    }
+
     return (
         <section className='signin-section'>
             <div className="container">
                 <div className="signin-wrapper">
                     <form className="signin-form" onSubmit={handleSubmit}>
+                        { isSignedIn ? <h2 className="form-title">Sing In</h2> : <h2 className="form-title">Create Account</h2>}
                         {
-                            isLoggedIn ? "" : <input type="text" name="name" onBlur={handleChange} placeholder="Name" className="form-control"/>
+                            isSignedIn ? "" : 
+                            <div className="input-container">
+                                <input type="text" name="name" onBlur={handleChange} placeholder="Name" className="form-control"/>
+                                <span className="input-alert">Enter your name</span>
+                            </div>
                         }
-                        <input type="email" name="email" onBlur={handleChange} placeholder="Email" className="form-control"/>
-                        <input type="password" name="password" onBlur={handleChange} placeholder="Password" className="form-control"/>
+                        <div className="input-container">
+                            <input type="email" name="email" onBlur={handleChange} placeholder="Email" className="form-control"/>
+                            <span className="input-alert">Enter your email</span>
+                        </div>
+
+                        <div className="input-container">
+                            <input type="password" name="password" onBlur={handleChange} placeholder="Password" className="form-control"/>
+                            <span className="input-alert">Password must be 6 characters</span>
+                        </div>
                         {
-                            isLoggedIn ? <input type="submit" value="Sign In" className="signin-btn"/> : <input type="submit" value="Sign Up" className="signin-btn"/>
+                            isSignedIn ? "" : 
+                            <div className="input-container">
+                                <input type="password" name="passwordCheck" onBlur={handleChange} placeholder="Re-enter Password" className="form-control"/>
+                                <span className="input-alert">Password not macthed</span>
+                            </div>
+                        }
+                        {
+                            isSignedIn ? <input type="submit" value="Sign In" className="signin-btn"/> : <input type="submit" value="Sign Up" className="signin-btn"/>
                         }
                     </form>
+
+                    <div className="social-singin">
+                        <button className="social-btn">
+                            <FcGoogle/>
+                            Sign Up with Google
+                        </button>
+                    </div>
+                    {
+                        isSignedIn ? <p>Don&apos;t have an account? <button className="toggleSignIn" onClick={toggleSignin}>Sign Up</button></p>
+                        : 
+                        <p>Already have an account? <button className="toggleSignIn" onClick={toggleSignin}>Sign In</button></p>
+                    }
                 </div>
             </div>
         </section>
     );
 };
 
-export default signin;
+export default Signin;
