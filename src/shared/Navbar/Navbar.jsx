beddb3f1b8862857bb29c97bfa03b256c450d536
rafikadir <rafikadir2000@gmail.com>
2023-10-31 11:08:28 +0600

    header updated with search function

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -4,54 +4,38 @@ import {AiOutlineShoppingCart } from "react-icons/ai";
 import logo from '../../assets/img/logo.png';
 import { useContext } from 'react';
 import { CartContext } from '../../App';
+import { FaUserAlt } from "react-icons/fa";
+import { BiSearchAlt } from "react-icons/bi";
 
 const Navbar = () => {
-
     const {cartProducts, isLoggedIn} = useContext(CartContext);
-
-    const navbarItems = [
-        {"name" : "Home", "link": "/"},
-        {"name" : "Shop", "link": "/shop"},
-        {"name" : "Contact", "link": "/contact"},
-        {"name" : "Account", "link": "/"}
-    ];
 
     return (
         <div className='header-wrapper'>
-           <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container">
-                    <Link className="navbar-brand" to="/">
-                        <img src={logo} alt="logo" />
-                    </Link>
+            <div className="header-top container">
+                <Link className="header-logo" to="/">
+                    <img src={logo} alt="logo" />
+                </Link>
 
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                <div className='header-search'>
+                    <input type="text" placeholder="Search"/>
+                    <button className='search-btn'>
+                        <BiSearchAlt/>
                     </button>
-
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav m-auto mb-2 mb-lg-0">
-                            {
-                                navbarItems.map((item, index) => (
-                                    <li className="nav-item" key={index}>
-                                        <Link className="nav-link" to={item.link}>{item.name}</Link>
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                    </div>
-
-                    <div className="header-right">
-                        <Link to="/cart" className="menu-cart">
-                            <AiOutlineShoppingCart />
-                            <span className='cart-item'>{cartProducts.length}</span>
-                        </Link>
-                        
-                        <Link to="/user" className="header-account">
-                            Account
-                        </Link>
-                    </div>
                 </div>
-            </nav>
+
+                <div className="header-right">
+                    <Link to="/cart" className="header-cart">
+                        <AiOutlineShoppingCart />
+                        <span className='cart-item'>{cartProducts.length}</span>
+                    </Link>
+                    
+                    <Link to="/user" className="header-account">
+                        <FaUserAlt/>
+                        My Account
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
